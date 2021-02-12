b0d6357914db1bd88bb483e414dc8cf964f48550
wonder-coconut <rrishab2001@gmail.com>
2021-02-13 00:08:48 +0530

    field priority sort progress, no priority sort yet

--- Lab2/sorting.c
+++ Lab2/sorting.c
@@ -260,8 +260,62 @@ int partition(struct team *teamset , int l , int r, struct team pivot, int ch)
             while(strcmp(teamset[left].name,pivot.name) <= 0)
                 left++;
 
-            while(strcmp(teamset[right].name,pivot.name) >= 0)
+            while(strcmp(teamset[right].name,pivot.name) > 0)
                 right--;
+        }
+        else if (ch == -1)
+        {
+            printf("left:%d\tright:%d\n",left,right);
+            while(1)
+            {
+                if(teamset[left].team > pivot.team)
+                    break;
+
+                else if(teamset[left].team == pivot.team)
+                {
+                    if(teamset[left].roll > pivot.team)
+                        break;
+
+                    else if(teamset[left].roll == pivot.roll)
+                    {
+                        if(strcmp(teamset[right].name,pivot.name) >= 0)
+                            break;
+                        
+                        else
+                            left++;
+                    }
+                    else
+                        left++;
+                }
+                else
+                    left++;
+            }
+
+            while(1)
+            {
+                printf("left:%d\tright:%d\n",left,right);
+                if(teamset[right].team < pivot.team)
+                    break;
+
+                else if(teamset[right].team == pivot.team)
+                {
+                    if(teamset[right].roll < pivot.team)
+                        break;
+
+                    else if(teamset[right].roll == pivot.roll)
+                    {
+                        if(strcmp(teamset[right].name,pivot.name) <= 0)
+                            break;
+                        
+                        else
+                            right--;
+                    }
+                    else
+                        right--;
+                }
+                else
+                    right--;
+            }
         }
 
         if(left >= right)
