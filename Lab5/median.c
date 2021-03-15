a295600d1ee6acb8b6cadf02d6e05ade2b1605a0
wonder-coconut <rrishab2001@gmail.com>
2021-03-15 11:33:08 +0530

    personalized median.c

--- Lab5/median.c
+++ Lab5/median.c
@@ -27,6 +27,7 @@ int nextGap(int gap)
         return 0;
     return (gap / 2) + (gap % 2);
 }
+
 void swap(int *n1, int *n2)
 {
     int temp = *n1;
@@ -34,30 +35,38 @@ void swap(int *n1, int *n2)
     *n2 = temp;
 }
 
-void merge(int* arr1, int* arr2, int n, int m)
+void merge(int ar1[] , int ar2[] , int n)
 {
-    int i, j, gap = n + m;
-    for (gap = nextGap(gap);gap > 0; gap = nextGap(gap)) 
+    int i,j,gap;
+    i = j = gap = 0;
+
+    for(gap = nextGap(2*n); gap > 0 ; gap = nextGap(gap))
     {
-        // comparing elements in the first array.
-        for (i = 0; i + gap < n; i++)
-            if (arr1[i] > arr1[i + gap])
-                swap(&arr1[i], &arr1[i + gap]);
- 
-        // comparing elements in both arrays.
-        for (j = gap > n ? gap - n : 0;i < n && j < m;i++, j++)
-            if (arr1[i] > arr2[j])
-                swap(&arr1[i], &arr2[j]);
- 
-        if (j < m) {
-            // comparing elements in the second array.
-            for (j = 0; j + gap < m; j++)
-                if (arr2[j] > arr2[j + gap])
-                    swap(&arr2[j], &arr2[j + gap]);
+        for(i = 0; i + gap < n ; i++)
+            if(ar1[i] > ar1[i+gap])
+                swap(&ar1[i] , &ar1[i+gap]);
+
+        if(gap > n)
+            j = gap - n;
+        else
+            j = 0;
+
+        while(i < n && j < n)
+        {
+            if(ar1[i] > ar2[j])
+                swap(&ar1[i],&ar2[j]);
+            
+            i++;
+            j++;
+        }
+
+        for(j = 0 ; j + gap < n ; j++)
+        {
+            if(ar2[j] > ar2[j+gap])
+                swap(&ar2[j], &ar2[j+gap]);
         }
     }
 }
-
 double median(int ar1[],int ar2[])
 {
     return (ar1[len-1] + ar2[0] + 0.0)/2;
@@ -88,13 +97,7 @@ int main(int argc , char *argv[])
 
     int arr[2*len];
 
-    printArray(ar1,len);
-    printArray(ar2,len);
-
-    merge(ar1,ar2,len,len);
-
-    printArray(ar1,len);
-    printArray(ar2,len);
+    merge(ar1,ar2,len);
 
     double res = median(ar1,ar2);
 
